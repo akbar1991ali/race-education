30e16f8bb49b472c19e0346aa7accf90646f4be9
akbar1991ali <akbar1991ali@yahoo.com>
2019-02-14 01:25:21 +0530

    [Header & Footer]

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+
+import { Directive, ElementRef, Renderer, HostListener } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  step = 0;
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  @HostListener('window:scroll', ['$event']) 
+  doSomething(event) {
+ 
+    if(window.pageYOffset >=80)
+    {
+      // console.log("Scroll Event", window.pageYOffset );
+this.step=87;
+    }else
+    {
+      this.step=0;
+    }
+  }
 }
